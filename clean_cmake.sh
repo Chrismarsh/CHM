e270a5a62dbceb1a8e2c2eafbb20027089742b3a
Chris Marsh <chrismarsh.c2@gmail.com>
2014-07-21 13:45:02 -0600

    fix missing -D flag for c compiler. added hardcode path to source the intel

--- clean_cmake.sh
+++ clean_cmake.sh
@@ -3,4 +3,5 @@
 CMakeCache.txt CMakeFiles/ Makefile cmake_install.cmake \
 src/CMakeFiles/ src/Makefile src/cmake_install.cmake\
 
-cmake -D CMAKE_CXX_COMPILER=icpc CMAKE_C_COMPILER=icc .
+source /opt/intel/bin/compilervars.sh intel64
+cmake -D CMAKE_CXX_COMPILER=icpc -D CMAKE_C_COMPILER=icc .
