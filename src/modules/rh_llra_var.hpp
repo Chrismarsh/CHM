80cc325d4590453d40125aa44edd3de842ee42f8
Chris Marsh <chrismarsh.c2@gmail.com>
2014-09-08 16:59:16 -0600

    Add documentation to rh llra var

--- src/modules/rh_llra_var.hpp
+++ src/modules/rh_llra_var.hpp
@@ -14,6 +14,38 @@
 
 #include <math.h>
 
+/**
+* \class rh_llra_var
+* \brief Linear lapse rate adjust for relative humidity
+*
+* Monthly-variable linear lapse rate adjustment for relative humidity based upon Liston, et al. (2006).
+*
+* Depends:
+* -  Air temperature "t" [deg C]
+*
+* Provides:
+* - Relative humidity "rh" [%]
+* - Relative humidity "rh_llra_var" [%]
+*
+* Reference:
+* > Liston, Glen E., and Kelly Elder. 2006. A meteorological distribution system for high-resolution terrestrial modeling (MicroMet). Journal of hydrometeorology 7: 217-234
+*
+* Month | Lapse (m^-1)
+* ------|-------
+* Jan | 0.00041
+* Feb | 0.00042
+* Mar | 0.00040
+* Apr | 0.00039
+* May | 0.00038
+* Jun | 0.00036
+* Jul | 0.00033
+* Aug | 0.00033
+* Sep | 0.00036
+* Oct | 0.00037
+* Nov | 0.00040
+* Dec | 0.00040
+
+*/
 class rh_llra_var : public module_base
 {
 public:
@@ -23,3 +55,5 @@ public:
 
 
 };
+
+
