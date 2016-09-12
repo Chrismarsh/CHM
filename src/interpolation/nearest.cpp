4dd46cf294ebf059427169bcc8cd7ddf5904193b
NicWayand <nicway@gmail.com>
2016-09-12 12:14:31 -0600

    bug fix

--- src/interpolation/nearest.cpp
+++ src/interpolation/nearest.cpp
@@ -12,17 +12,13 @@ nearest::~nearest()
 
 double nearest::operator()(std::vector< boost::tuple<double,double,double> >& sample_points, boost::tuple<double,double,double>& query_point)
 {
-    
-    double numerator = 0.0;
-    double denominator = 0.0;
 
-    double z0 = 0;
     if (sample_points.size() > 1)
     {
         BOOST_THROW_EXCEPTION( interpolation_error()
                                 << errstr_info("nearest requires exactly 1 station"));
     }
 
-    return z = sample_points.at(i).get<2>();
+    return sample_points.at(0).get<2>();
 
 }
