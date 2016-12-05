d17196dd8abda83d304a60b174f6c325eede370a
Chris Marsh <chrismarsh.c2@gmail.com>
2016-12-05 17:39:54 -0600

    update includes
    remove unneeded unned sundails incl
    update gdal to require 2.1

--- src/modules/dbsm.hpp
+++ src/modules/dbsm.hpp
@@ -12,11 +12,11 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-#include <cvode/cvode.h>             /* prototypes for CVODE fcts., consts. */
-#include <nvector/nvector_serial.h>  /* serial N_Vector types, fcts., macros */
-#include <cvode/cvode_dense.h>       /* prototype for CVDense */
-#include <sundials/sundials_dense.h> /* definitions DlsMat DENSE_ELEM */
-#include <sundials/sundials_types.h> /* definition of type realtype */
+//#include <cvode/cvode.h>             /* prototypes for CVODE fcts., consts. */
+//#include <nvector/nvector_serial.h>  /* serial N_Vector types, fcts., macros */
+//#include <cvode/cvode_dense.h>       /* prototype for CVDense */
+//#include <sundials/sundials_dense.h> /* definitions DlsMat DENSE_ELEM */
+//#include <sundials/sundials_types.h> /* definition of type realtype */
 
 /* User-defined vector and matrix accessor macros: Ith, IJth */
 
