c88f366e7be0eb109e5f05fc3598c569393316c0
NicWayand <nicway@gmail.com>
2016-12-19 21:26:46 -0600

    Fixed param only processed once

--- tools/vtu2geo/main.py
+++ tools/vtu2geo/main.py
@@ -55,7 +55,7 @@ def main():
     parameters = []
     if hasattr(X,'parameters'):
         parameters = X.parameters
-
+    
     # Check if we want to constrain output to a example geotif
     constrain_flag = False
     if hasattr(X,'constrain_tif_file'):
@@ -198,11 +198,11 @@ def main():
             for v in variables:
                 data = cd.GetArray(v).GetTuple(i)
                 feature.SetField(str(v), float(data[0]))
-
-
-            for p in parameters:
-                data = cd.GetArray(p).GetTuple(i)
-                feature.SetField(str(p), float(data[0]))
+	    
+	    if parameters is not None:
+            	for p in parameters:
+                	data = cd.GetArray(p).GetTuple(i)
+                	feature.SetField(str(p), float(data[0]))
 
 
             layer.CreateFeature(feature)
@@ -232,20 +232,21 @@ def main():
 	    if(constrain_flag):
                 subprocess.check_call(['gdalwarp -overwrite -s_srs \"%s\" -t_srs \"%s\" -te %f %f %f %f \"%s\" \"%s\"' % (srsout.ExportToProj4(),srsout.ExportToProj4(),o_xmin, o_ymin, o_xmax, o_ymax, path[:-4]+'_'+var+'.tif',path[:-4]+'_'+var+'_clipped.tif')], shell=True)
 
-        for p in parameters:
-            target_ds = gdal.GetDriverByName('GTiff').Create(path[:-4] + '_' + p.replace(" ","_") + '.tif', x_res, y_res, 1, gdal.GDT_Float32)
-            target_ds.SetGeoTransform((x_min, pixel_size, 0, y_max, 0, -pixel_size))
-            target_ds.SetProjection(srsout.ExportToWkt())
-            band = target_ds.GetRasterBand(1)
-            band.SetNoDataValue(NoData_value)
+	    if parameters is not None:
+		for p in parameters:
+		    target_ds = gdal.GetDriverByName('GTiff').Create(path[:-4] + '_' + p.replace(" ","_") + '.tif', x_res, y_res, 1, gdal.GDT_Float32)
+		    target_ds.SetGeoTransform((x_min, pixel_size, 0, y_max, 0, -pixel_size))
+		    target_ds.SetProjection(srsout.ExportToWkt())
+		    band = target_ds.GetRasterBand(1)
+		    band.SetNoDataValue(NoData_value)
 
-            # Rasterize
-            gdal.RasterizeLayer(target_ds, [1], layer, burn_values=[0],options=['ALL_TOUCHED=TRUE', "ATTRIBUTE=" + p])
-            target_ds = None
+		    # Rasterize
+		    gdal.RasterizeLayer(target_ds, [1], layer, burn_values=[0],options=['ALL_TOUCHED=TRUE', "ATTRIBUTE=" + p])
+		    target_ds = None
 
-            # Optional clip file
-            if(constrain_flag):
-                subprocess.check_call(['gdalwarp -overwrite -s_srs \"%s\" -t_srs \"%s\" -te %f %f %f %f \"%s\" \"%s\"' % (srsout.ExportToProj4(),srsout.ExportToProj4(),o_xmin, o_ymin, o_xmax, o_ymax,path[:-4] + '_' + p.replace(" ","_") + '.tif',path[:-4] + '_' + p.replace(" ","_") + '_clipped.tif')], shell=True)
+		    # Optional clip file
+		    if(constrain_flag):
+			subprocess.check_call(['gdalwarp -overwrite -s_srs \"%s\" -t_srs \"%s\" -te %f %f %f %f \"%s\" \"%s\"' % (srsout.ExportToProj4(),srsout.ExportToProj4(),o_xmin, o_ymin, o_xmax, o_ymax,path[:-4] + '_' + p.replace(" ","_") + '.tif',path[:-4] + '_' + p.replace(" ","_") + '_clipped.tif')], shell=True)
 	    
         #we don't need to dump parameters for each timestep as they are currently assumed invariant with time.
         parameters = None
