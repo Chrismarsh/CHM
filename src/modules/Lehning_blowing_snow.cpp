7ecd5ff78e62730a7c420352e6657bb99507c314
Chris marsh <chrismarsh.c2@gmail.com>
2017-01-12 10:18:13 -0600

    before re-write to add layers

--- src/modules/Lehning_blowing_snow.cpp
+++ src/modules/Lehning_blowing_snow.cpp
@@ -30,6 +30,8 @@ Lehning_blowing_snow::Lehning_blowing_snow(config_file cfg)
     provides("l2");
     provides("u_z");
 
+    provides("Qsusp");
+
 }
 
 void Lehning_blowing_snow::init(mesh domain)
@@ -51,284 +53,255 @@ void Lehning_blowing_snow::run(mesh domain)
     //needed for linear system offsets
     size_t ntri = domain->number_of_faces();
 
-//    arma::sp_mat C(ntri * nLayer , ntri * nLayer ) ;
+//    arma::sp_mat C(ntri , ntri * nLayer ) ;
     arma::mat C(ntri * nLayer , ntri * nLayer, arma::fill::zeros) ;
     arma::vec b(ntri * nLayer , arma::fill::zeros);
     arma::vec x(ntri * nLayer , arma::fill::zeros);
     double z0 = 0.01; //m
 
-
-    for(int itr = 0; itr < 1; itr++)
+    for (size_t i = 0; i < domain->size_faces(); i++)
     {
-        C.fill(0);
-        for (size_t i = 0; i < domain->size_faces(); i++)
+        auto face = domain->face(i);
+
+        double T = face->face_data("t");
+        double phi =face->face_data("vw_dir");
+        double u2 = face->face_data("U_2m_above_srf");
+
+
+        arma::vec u(2);
+        Vector_2 v = math::gis::bearing_to_cartesian(phi);
+        u(0) = -v.x(); //U_x
+        u(1) = -v.y(); //U_y
+
+        //edge unit normal
+        arma::vec m[5];
+        m[0].set_size(3);
+        m[0](0) = face->edge_unit_normal(0).x();
+        m[0](1) = face->edge_unit_normal(0).y();
+        m[0](2) = 0;
+
+        m[1].set_size(3);
+        m[1](0) = face->edge_unit_normal(1).x();
+        m[1](1) = face->edge_unit_normal(1).y();
+        m[1](2) = 0;
+
+        m[2].set_size(3);
+        m[2](0) = face->edge_unit_normal(2).x();
+        m[2](1) = face->edge_unit_normal(2).y();
+        m[2](2) = 0;
+
+        //top
+        m[3].set_size(3);
+        m[3].fill(0);
+        m[3](2) = 1;
+
+        //bottom
+        m[4].set_size(3);
+        m[4].fill(0);
+        m[4](2) = -1;
+
+        //face areas
+        double A[5];
+        for (int j = 0; j < 3; ++j)
+            A[j] = face->edge_length(j); //assuming 1m height
+
+        //top, bottom
+        A[3] = A[4] = face->get_area();
+
+
+        //depth of saltation layer
+        double hs = z0 + 2.4025 * pow(u2, 2.) * pow(PhysConst::kappa, 2.) * pow(cos(25. * M_PI / 180.), 2.) /
+                         (pow(log(2. / z0), 2.) * 9.81);
+        face->set_face_data("hs", hs);
+
+
+        double ustar = std::max(0.1, u2 * PhysConst::kappa / log(2. / z0));
+        face->set_face_data("ustar", ustar);
+
+
+        // iterate over the vertical layers, each 1m in height
+        for (int z = 0; z < nLayer; ++z)
         {
-            auto face = domain->face(i);
-
-            double T = face->face_data("t");
-            double phi = face->face_data("vw_dir");
-            double u2 = face->face_data("U_2m_above_srf");
+            //height of the suspension layer
+            double cz = z + hs; //cell height
 
 
-            arma::vec u(2);
-            Vector_2 v = math::gis::bearing_to_cartesian(phi);
-            u(0) = v.x(); //U_x
-            u(1) = v.y(); //U_y
+            double l_max = 1;
 
-            //edge unit normal
-            arma::vec m[5];
-            m[0].set_size(3);
-            m[0](0) = face->edge_unit_normal(0).x();
-            m[0](1) = face->edge_unit_normal(0).y();
-            m[0](2) = 0;
+            double K[5];
 
-            m[1].set_size(3);
-            m[1](0) = face->edge_unit_normal(1).x();
-            m[1](1) = face->edge_unit_normal(1).y();
-            m[1](2) = 0;
+            // holds A_ * K_ / h_
+            // _0 -> _2 are the horizontal sides
+            // _3 -> is the top of the prism
+            // _4 -> is the bottom the prism
+            double alpha[5];
 
-            m[2].set_size(3);
-            m[2](0) = face->edge_unit_normal(2).x();
-            m[2](1) = face->edge_unit_normal(2).y();
-            m[2](2) = 0;
+            //which faces have neighbours? Ie, are we an edge
+            bool face_neigh[3] = {true, true, true};
 
-            //top
-            m[3].set_size(3);
-            m[3].fill(0);
-            m[3](2) = 1;
+            double l__max = 1;
 
-            //bottom
-            m[4].set_size(3);
-            m[4].fill(0);
-            m[4](2) = -1;
-
-            //face areas
-            double A[5];
-            for (int j = 0; j < 3; ++j)
-                A[j] = face->edge_length(j); //assuming 1m height
-
-            //top, bottom
-            A[3] = A[4] = face->get_area();
-
-
-            //depth of saltation layer
-            double hs = z0 + 2.4025 * pow(u2, 2.) * pow(PhysConst::kappa, 2.) * pow(cos(25. * M_PI / 180.), 2.) /
-                             (pow(log(2. / z0), 2.) * 9.81);
-            face->set_face_data("hs", hs);
-
-
-            double ustar = std::max(0.1, u2 * PhysConst::kappa / log(2. / z0));
-            face->set_face_data("ustar", ustar);
-//
-//        if(ustar < .9)
-//            c_salt = 0;
-
-
-            // iterate over the vertical layers, each 1m in height
-            for (int z = 0; z < nLayer; ++z)
+            //compute alpha and K
+            for (int a = 0; a < 3; ++a)
             {
-                //height of the suspension layer
-                double cz = z + hs; //cell height
+                auto neigh = face->neighbor(a);
+                alpha[a] = A[a];
 
-
-                double l_max = 1;
-
-                double K[5];
-
-                // holds A_ * K_ / h_
-                // _0 -> _2 are the horizontal sides
-                // _3 -> is the top of the prism
-                // _4 -> is the bottom the prism
-                double alpha[5];
-
-                //which faces have neighbours? Ie, are we an edge
-                bool face_neigh[3] = {true, true, true};
-
-                double l__max = 1;
-
-                //compute alpha and K
-                for (int a = 0; a < 3; ++a)
+                //if we have a neighbour, use the distance
+                if (neigh != nullptr)
                 {
-                    auto neigh = face->neighbor(a);
-                    alpha[a] = A[a];
+                    l__max = math::gis::distance(face->center(), neigh->center());
 
-                    //if we have a neighbour, use the distance
-                    if (neigh != nullptr)
-                    {
-                        l__max = math::gis::distance(face->center(), neigh->center());
+                    alpha[a] /= math::gis::distance(face->center(), neigh->center());
 
-                        alpha[a] /= math::gis::distance(face->center(), neigh->center());
+                } else
+                { //otherwise assume 2x the distance from the center of face to one of it's vertexes, so ghost triangle is approx the same size
+                    alpha[a] /= 2.0 * math::gis::distance(face->center(), face->vertex(0)->point());
 
-                    } else
-                    { //otherwise assume 2x the distance from the center of face to one of it's vertexes, so ghost triangle is approx the same size
-                        alpha[a] /= 2.0 * math::gis::distance(face->center(), face->vertex(0)->point());
-
-                        face_neigh[a] = false;
-                    }
-
-//                double z = 2; // this used to depend on the layer depth, but I think it is supposed to be constant as u* is const w/ height.
-                    double l = PhysConst::kappa * (cz + z0) * l__max /
-                               (PhysConst::kappa * z + PhysConst::kappa * z0 + l__max);
-                    K[a] = 0;// std::max(ustar * l, PhysConst::kappa * 2. * ustar);
-
-                    //no horizontal diffusion ..... kek
-                    alpha[a] *= K[a];
+                    face_neigh[a] = false;
                 }
 
-                l__max = 40;
+//                double z = 2; // this used to depend on the layer depth, but I think it is supposed to be constant as u* is const w/ height.
+                double l = PhysConst::kappa * (cz + z0) * l__max /
+                           (PhysConst::kappa * z + PhysConst::kappa * z0 + l__max);
+                K[a] = 0;// std::max(ustar * l, PhysConst::kappa * 2. * ustar);
 
-                double l =
-                        PhysConst::kappa * (cz + z0) * l__max / (PhysConst::kappa * z + PhysConst::kappa * z0 + l__max);
+                //no horizontal diffusion ..... kek
+                alpha[a] *= K[a];
+            }
+
+            l__max = 40;
+
+            double l =
+                    PhysConst::kappa * (cz + z0) * l__max / (PhysConst::kappa * z + PhysConst::kappa * z0 + l__max);
 //            double l = pow( 1./(PhysConst::kappa *(cz+z0))+1.0/l__max,-2.0);
-                K[3] = K[4] = std::max(ustar * l, PhysConst::kappa * 2. * ustar);
+            K[3] = K[4] = std::max(ustar * l, PhysConst::kappa * 2. * ustar);
 
 
-                //top
-                alpha[3] = A[3] * K[3] / (susp_depth / nLayer);
-                //bottom
-                alpha[4] = A[4] * K[4] / (susp_depth / nLayer);
+            //top
+            alpha[3] = A[3] * K[3] / (susp_depth / nLayer);
+            //bottom
+            alpha[4] = A[4] * K[4] / (susp_depth / nLayer);
 
-                face->set_face_data("K0", K[0]);
-                face->set_face_data("K1", K[1]);
-                face->set_face_data("K2", K[2]);
-                face->set_face_data("K3", K[3]);
-                face->set_face_data("K4", K[4]);
+            face->set_face_data("K0", K[0]);
+            face->set_face_data("K1", K[1]);
+            face->set_face_data("K2", K[2]);
+            face->set_face_data("K3", K[3]);
+            face->set_face_data("K4", K[4]);
 
-                face->set_face_data("l_vert", l);
+            face->set_face_data("l_vert", l);
 
-                //concentration of snow at reference height in saltation layer
-                //Pomeroy et al 1993, p.169
-                double nzr = 0.8; // kg/m^3
+            //concentration of snow at reference height in saltation layer
+            //Pomeroy et al 1993, p.169
+            double nzr = 0.8; // kg/m^3
 
-                double c_salt = 0;//nzr;
-                if (i == 0)
-                    c_salt = 0.8;
-//            double c_salt = nzr*exp(-1.55*(1./pow(0.05628*ustar,.544)-1./pow(cz,.544)));
+//            double c_salt = 0.;
+//            if (i == 0 )//|| i== 2440 || i==1902 || i==1903)
+//                c_salt = .80;
+            double c_salt = nzr*exp(-1.55*(1./pow(0.05628*ustar,.544)-1./pow(cz,.544)));
 
-                face->set_face_data("csalt", c_salt);
+            face->set_face_data("csalt", c_salt);
 
-                //compute new U_z
-                double u_z = std::max(0.1, Atmosphere::log_scale_wind(u2, 2, cz, 0));
+            //compute new U_z
+            double u_z =std::max(0.1, Atmosphere::log_scale_wind(u2, 2, cz, 0));
 
-                arma::vec uvw(3);
-                uvw(0) = u(0);
-                uvw(1) = u(1);
-                uvw(2) = 0;
+            arma::vec uvw(3);
+            uvw(0) = u(0);
+            uvw(1) = u(1);
+            uvw(2) = 0;
 
-                double length = arma::norm(uvw, 2);
-                double scale = u_z / length;
+            double length = arma::norm(uvw, 2);
+            double scale = u_z / length;
 
-                //calculate u_z at this height in the suspension layer
-                uvw *= scale;
+            //calculate u_z at this height in the suspension layer
+            uvw *= scale;
 //
 //            uvw(2) = -.5;
 
 
-                //holds wind dot face normal
-                double udotm[5];
-                for (int j = 0; j < 5; ++j)
+            //holds wind dot face normal
+            double udotm[5];
+            for (int j = 0; j < 5; ++j)
+            {
+                udotm[j] = arma::dot(uvw, m[j]);
+            }
+
+            //lateral
+
+            if (face_neigh[0])
+            {
+                C(i+ntri*z, ntri * z + face->cell_id) +=  -1. * alpha[0] - .5000000000 * A[0] * udotm[0];
+
+                auto neigh = face->neighbor(0);
+                C(i*z, ntri * z + neigh->cell_id) += alpha[0] - .5000000000 * A[0] * udotm[0];
+
+            }
+            if (face_neigh[1])
+            {
+                C(i*z, ntri * z + face->cell_id) +=  -1. * alpha[1] - .5000000000 * A[1] * udotm[1];
+
+                auto neigh = face->neighbor(1);
+                C(i*z, ntri * z + neigh->cell_id) +=alpha[1] - .5000000000 * A[1] * udotm[1];
+            }
+            if (face_neigh[2])
+            {
+                C(i*z, ntri * z + face->cell_id) +=  -1. * alpha[2] - .5000000000 * A[2] * udotm[2];
+
+                auto neigh = face->neighbor(2);
+                C(i*z, ntri * z + neigh->cell_id) += alpha[2] - .5000000000 * A[2] * udotm[2];
+            }
+
+            //top
+            if (nLayer == 1)
+            {
+
+                C(i*z, ntri * z + face->cell_id) += -1.*alpha[3]+1.*A[4]*K[4]/(.5000000000*hs+.5)-1.*K[4];
+
+                b(ntri * z + face->cell_id) = 1.*A[4]*K[4]*c_salt/(.5000000000*hs+.5);//-1.*K[4]*c_salt;
+                //top face
+                //0
+            } else
+            {
+                //now do the vertical fluxes
+                //we are the bottom?
+                if (z == 0)
                 {
-                    udotm[j] = arma::dot(uvw, m[j]);
-                }
-
-
-                //check if we are an interior (w.r.t lateral) cell
-//            if(face_neigh[0] && face_neigh[1] && face_neigh[2] )
-//            {
-//                //we are interior
-//
-//                C(i, ntri * z + face->cell_id) =
-//                        -1.*alpha[0]-1.*alpha[1]-1.*alpha[2]-.5000000000*A[0]*udotm[0]-.5000000000*A[1]*udotm[1]-.5000000000*A[2]*udotm[2];
-//
-//
-//                //do edges
-//                for(int j = 0; j<3; ++j)
-//                {
-//                    auto neigh = face->neighbor(j);
-//                    if (neigh != nullptr)
-//                    {
-//                        C(i,  ntri * z + neigh->cell_id) = alpha[j]-.5000000000*A[j]*udotm[j];
-//                    }
-//                    //0 flux BC if not
-//                }
-//
-//            }
-                //lateral
-
-                if (face_neigh[0])
-                {
-                    C(i, ntri * z + face->cell_id) +=  -1. * alpha[0] - .5000000000 * A[0] * udotm[0];
-
-                    auto neigh = face->neighbor(0);
-                    C(i, ntri * z + neigh->cell_id) += itr != 0? x(ntri * z + neigh->cell_id) : alpha[0] - .5000000000 * A[0] * udotm[0];
-
-                }
-                if (face_neigh[1])
-                {
-                    C(i, ntri * z + face->cell_id) +=  -1. * alpha[1] - .5000000000 * A[1] * udotm[1];
-
-                    auto neigh = face->neighbor(1);
-                    C(i, ntri * z + neigh->cell_id) += itr != 0? x(ntri * z + neigh->cell_id):alpha[1] - .5000000000 * A[1] * udotm[1];
-                }
-                if (face_neigh[2])
-                {
-                    C(i, ntri * z + face->cell_id) +=  -1. * alpha[2] - .5000000000 * A[2] * udotm[2];
-
-                    auto neigh = face->neighbor(2);
-                    C(i, ntri * z + neigh->cell_id) += itr != 0? x(ntri * z + neigh->cell_id):alpha[2] - .5000000000 * A[2] * udotm[2];
-                }
-
-                //top
-                if (nLayer == 1)
-                {
-
+                    //bottom face
                     C(i, ntri * z + face->cell_id) += -1.*alpha[3]+1.*A[4]*K[4]/(.5000000000*hs+.5)-1.*K[4];
+                    b(ntri * z + face->cell_id) =
+                            1.*A[4]*K[4]*c_salt/(.5000000000*hs+.5);
 
-                    b(ntri * z + face->cell_id) = 1.*A[4]*K[4]*c_salt/(.5000000000*hs+.5);//-1.*K[4]*c_salt;
                     //top face
-                    //0
-                } else
+                    C(i, ntri * z + face->cell_id) += -1. * alpha[3] - .5000000000 * A[3] * udotm[3];
+                    C(i, ntri * (z + 1) + face->cell_id) = (alpha[3] - .5000000000 * A[3] * udotm[3]);
+                } else if (z == nLayer - 1)// top z layer
                 {
-                    //now do the vertical fluxes
-                    //we are the bottom?
-                    if (z == 0)
-                    {
-                        //bottom face
-                        C(i, ntri * z + face->cell_id) += A[4] * K[4] + 1. * A[4] * K[4] / (.5000000000 * hs + .5);
-                        b(ntri * z + face->cell_id) =
-                                1. * A[4] * K[4] * c_salt + 1. * A[4] * K[4] * c_salt / (.5000000000 * hs + .5);
+                    //top is neumann b.c, del c = 0
+                    //only bottom
+                    C(i, ntri * z + face->cell_id) +=
+                            -1. * alpha[3] - 1. * alpha[4] - .5000000000 * A[3] * udotm[3] -
+                            .5000000000 * A[4] * udotm[4];
+                    C(i, ntri * (z - 1) + face->cell_id) = (alpha[4] - .5000000000 * A[4] * udotm[4]);
 
-                        //top face
-                        C(i, ntri * z + face->cell_id) += -1. * alpha[3] - .5000000000 * A[3] * udotm[3];
-                        C(i, ntri * (z + 1) + face->cell_id) = (alpha[3] - .5000000000 * A[3] * udotm[3]);
-                    } else if (z == nLayer - 1)// top z layer
-                    {
-                        //top is neumann b.c, del c = 0
-                        //only bottom
-                        C(i, ntri * z + face->cell_id) +=
-                                -1. * alpha[3] - 1. * alpha[4] - .5000000000 * A[3] * udotm[3] -
-                                .5000000000 * A[4] * udotm[4];
-                        C(i, ntri * (z - 1) + face->cell_id) = (alpha[4] - .5000000000 * A[4] * udotm[4]);
+                    //                C(i, ntri * z + face->cell_id) += -1.*alpha[3]-1.*alpha[4]-.5000000000*A[3]*udotm[3]-.5000000000*A[4]*udotm[3];
+                    //                C(i, ntri * (z-1) + face->cell_id) =  alpha[4]-.5000000000*A[4]*udotm[3];
+                } else // internal cell
+                {
+                    C(i, ntri * z + face->cell_id) +=
+                            -1. * alpha[3] - 1. * alpha[4] - .5000000000 * A[3] * udotm[3] -
+                            .5000000000 * A[4] * udotm[4];
 
-                        //                C(i, ntri * z + face->cell_id) += -1.*alpha[3]-1.*alpha[4]-.5000000000*A[3]*udotm[3]-.5000000000*A[4]*udotm[3];
-                        //                C(i, ntri * (z-1) + face->cell_id) =  alpha[4]-.5000000000*A[4]*udotm[3];
-                    } else // internal cell
-                    {
-                        C(i, ntri * z + face->cell_id) +=
-                                -1. * alpha[3] - 1. * alpha[4] - .5000000000 * A[3] * udotm[3] -
-                                .5000000000 * A[4] * udotm[4];
+                    //top
+                    C(i, ntri * (z + 1) + face->cell_id) = (alpha[3] - .5000000000 * A[3] * udotm[3]);
 
-                        //top
-                        C(i, ntri * (z + 1) + face->cell_id) = (alpha[3] - .5000000000 * A[3] * udotm[3]);
-
-                        //bottom
-                        C(i, ntri * (z - 1) + face->cell_id) = (alpha[4] - .5000000000 * A[4] * udotm[4]);
-                    }
+                    //bottom
+                    C(i, ntri * (z - 1) + face->cell_id) = (alpha[4] - .5000000000 * A[4] * udotm[4]);
                 }
             }
         }
-        x = arma::solve(C,b);
     }
+    x = arma::solve(C,b);
+
 
 //    LOG_DEBUG << "Cond: " << arma::cond(C);
 //    arma::mat L, U, P;
@@ -346,7 +319,7 @@ void Lehning_blowing_snow::run(mesh domain)
 //    U.reset();
 //    P.reset();
 
-//    arma::vec x = arma::spsolve(C,b);
+//    x = arma::spsolve(C,b);
 
     for (size_t i = 0; i < domain->size_faces(); i++)
     {
