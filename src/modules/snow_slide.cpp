b795d8443a088b763de31fa07f1a2126e7928cb5
NicWayand <nicway@gmail.com>
2017-01-24 14:24:27 -0600

    added snowdepth to height calcs. Weighted routing of snow.

--- src/modules/snow_slide.cpp
+++ src/modules/snow_slide.cpp
@@ -20,27 +20,26 @@ snow_slide::~snow_slide()
 void snow_slide::run(mesh domain)
 {
 
-    // Make a vector of pairs (elevation, pointer to face)
+    // Make a vector of pairs (elevation + snowdepth, pointer to face)
     std::vector< std::pair<double, mesh_elem> > sorted_z;
     for(int i = 0; i  <domain->size_faces(); i++)
     {
         auto face = domain->face(i);
-        sorted_z.push_back( std::make_pair( face->center().z(), face) );
+        sorted_z.push_back( std::make_pair( face->center().z() + face->face_data("snowdepthavg"), face) );
 
     }
 
-    // Sort faces by elevation (TODO: add snowpack height)
+    // Sort faces by elevation + snowdepth
     std::sort(sorted_z.begin(), sorted_z.end(), [](std::pair<double, mesh_elem> &a, std::pair<double, mesh_elem> &b) {
         return b.first < a.first;
     });
 
-
-    // Loop through each face, from highest to lowest triangle
+    // Loop through each face, from highest to lowest triangle surface
     for (size_t i = 0; i < sorted_z.size(); i++)
     {
-    	auto face = sorted_z[i].second;
+    	auto face = sorted_z[i].second; // Get pointer to face
 
-	auto data = face->get_module_data<snow_slide::data>(ID);
+	auto data = face->get_module_data<snow_slide::data>(ID); // Get stored data for face
 
 	// Loop through each face, from highest to lowest triangle
 	double maxDepth = data->maxDepth;
@@ -50,31 +49,38 @@ void snow_slide::run(mesh domain)
 	// Check if face snowdepth have exceeded maxDepth
         if (snowdepthavg > maxDepth) {
 		LOG_DEBUG << "avalanche! " << snowdepthavg << " " << maxDepth;
-                // Get elevations of neighbors
-		double min_z = 99999.0; // A large number
-                int min_i = 10; // something larger than 2
-		for(int i = 0; i < 3; ++i) {
+                // Calc weights for routing snow
+		
+		double z_s = face->center().z() + snowdepthavg; // Current face elevation + snowdepth
+		std::vector<double> w = {0,0,0}; // Weights for each face neighbor to route snow to
+                double w_dem = 0; // Denomenator for weights (sum of all elev diffs)
+                for(int i = 0; i < 3; ++i) {
 		    auto n = face->neighbor(i);
     
 		    // If not null (edge case) check is less than lowFace
     		    if(n != nullptr) {
-			if(n->center().z() < min_z) { 
-			    min_z = n->center().z();
-			    min_i = i;
-			}
+			w[i] = z_s -  (n->center().z()+n->face_data("snowdepthavg")); // Calc weighting based on height diff
+                        w_dem += w[i]; // Store weight denominator 
 		    }
-	        }
-		auto n0 = face->neighbor(min_i);
-		//LOG_DEBUG << "neighbor elev " << n0->center().z() << ". min_z " << min_z;
+		}
+                // Divide by sum height differences to create weights that sum to unity
+                std::transform(w.begin(), w.end(), w.begin(),
+                   [w_dem](double cw) { return cw/w_dem; });
+	
 		
-		// Route snow to neighbor
-		n0->set_face_data("snowdepthavg",n0->face_data("snowdepthavg")+snowdepthavg);
-                n0->set_face_data("swe",n0->face_data("swe")+swe);
-
-		// Remove snow from initial face
-		face->set_face_data("snowdepthavg",0.0);
+		// Route snow to each neighbor based on weights
+                // TODO: Include vegetation heigh in roughting calcs
+		for(int i = 0; i < 3; ++i) {
+                    auto n = face->neighbor(i);
+                    if(n != nullptr) {
+                        n->set_face_data("snowdepthavg",n->face_data("snowdepthavg")+snowdepthavg*w[i]);
+                        n->set_face_data("swe",n->face_data("swe")+swe*w[i]);
+                    }
+		}
+                // Remove snow from initial face (TODO: should we only remove above maxDepth??)
+                face->set_face_data("snowdepthavg",0.0);
                 face->set_face_data("swe",0.0);
-
+		
         } 
 
 	// Do nothing if threshold maxDepth not exceded 
