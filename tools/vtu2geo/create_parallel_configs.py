4fe0d1bf3aef9fe18266a5d192ba6b6aa17c1a18
NicWayand <nicway@gmail.com>
2017-03-24 17:22:17 -0600

    working parallel version of vtu2geo

--- tools/vtu2geo/create_parallel_configs.py
+++ tools/vtu2geo/create_parallel_configs.py
@@ -6,27 +6,9 @@ import sys
 import xml.etree.ElementTree as ET
 import subprocess
 import numpy as np
+import os
+import shutil
 gdal.UseExceptions()  # Enable errors
-
-# Print iterations progress
-# http://stackoverflow.com/a/34325723/410074
-def printProgress(iteration, total, prefix='', suffix='', decimals=2, barLength=100):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iterations  - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-    """
-    filledLength = int(round(barLength * iteration / float(total)))
-    percents = round(100.00 * (iteration / float(total)), decimals)
-    bar = '#' * filledLength + '-' * (barLength - filledLength)
-    sys.stdout.write('%s [%s] %s%s %s\r' % (prefix, bar, percents, '%', suffix)),
-    sys.stdout.flush()
-    if iteration == total:
-        print("\n")
-
 
 def main():
     gdal.UseExceptions()  # Enable errors
@@ -42,7 +24,7 @@ def main():
 
     # Load in configuration file as module
     X = imp.load_source('',configfile)
-
+    
     # Grab variables
     input_path = X.input_path
 
@@ -50,6 +32,14 @@ def main():
     parameters = []
     if hasattr(X,'parameters'):
         parameters = X.parameters
+
+    #config_dir = 'parallel_configs'
+    if hasattr(X,'config_dir'):
+	config_dir = X.config_dir
+	
+    # Create folder if it doesn't exist
+    if not os.path.exists(config_dir):
+    	os.makedirs(config_dir)
 
     # Check if we want to constrain output to a example geotif
     constrain_flag = False
@@ -79,192 +69,34 @@ def main():
     pvd = ET.parse(input_path)
     pvd = pvd.findall(".//*[@file]")
 
-    # Get info for constrained output extent/resolution if selected
-    if(constrain_flag):
-        from gdalconst import GA_ReadOnly
-        ex_ds = gdal.Open(constrain_tif_file,GA_ReadOnly)
-        gt = ex_ds.GetGeoTransform()
-        pixel_width = np.abs(gt[1])
-        pixel_height = np.abs(gt[5])
-        # Take extent from user input
-        if user_define_extent:
-            o_xmin = X.o_xmin
-            o_xmax = X.o_xmax
-            o_ymin = X.o_ymin
-            o_ymax = X.o_ymax
-        else: # Get extent for clipping from input tif
-            o_xmin = gt[0]
-            o_ymax = gt[3]
-            o_xmax = o_xmin + gt[1] * ex_ds.RasterXSize
-            o_ymin = o_ymax + gt[5] * ex_ds.RasterYSize
+    # Create execution file
+    f2 = open(config_dir+'/parallel_commands.txt','w')
 
-        print "Output pixel size is " + str(pixel_width) + " by " + str(pixel_height)
-        ex_ds = None
+    # TODO: hardcoded
+    para_ex = '/home/new365/CHM/tools/vtu2geo/main_parallel.py'
 
-
-
-    iter=1
     for vtu in pvd:
 
+	# Get full file name
         vtu_file  = vtu.get('file')
-        path = input_path[:input_path.rfind('/')+1] + vtu_file
-        printProgress(iter,len(pvd))
-        reader.SetFileName(path)
-        reader.Update()
+        
+	# Copy templet config file
+	new_config = config_dir+'/cfg_'+vtu_file+'.txt'
+	shutil.copy(configfile, new_config)	
 
-        mesh = reader.GetOutput()
+	# Open config file for writing
+	f = open(new_config, 'a')
 
-        #default the pixel size to (min+max)/2
-        if not pixel_size:
-            area_range = mesh.GetCellData().GetArray('Area').GetRange()
-            pixel_size = (math.sqrt(area_range[0]) + math.sqrt(area_range[1]))/2
-            pixel_size = int( math.ceil(pixel_size) )
+	# Write file name
+	f.write('vtu_file="'+vtu_file+'"\n')
 
-        driver = ogr.GetDriverByName('Memory')
-        output_usm = driver.CreateDataSource('out')
+	# Close config file
+	f.close()
 
-        srsin = osr.SpatialReference()
-
-        if not mesh.GetFieldData().HasArray("proj4"):
-            print "VTU file does not contain a proj4 field"
-            return -1
-
-        vtu_proj4 = mesh.GetFieldData().GetAbstractArray("proj4").GetValue(0)
-        srsin.ImportFromProj4(vtu_proj4)
-
-        is_geographic = srsin.IsGeographic()
-
-        #output conic equal area for geotiff
-        srsout = osr.SpatialReference()
-
-        if not is_geographic:
-            srsout.ImportFromProj4(vtu_proj4)
-        else:
-            srsout.ImportFromWkt("PROJCS[\"North_America_Albers_Equal_Area_Conic\",     "
-                                 "GEOGCS[\"GCS_North_American_1983\",         "
-                                 "DATUM[\"North_American_Datum_1983\",            "
-                                 " SPHEROID[\"GRS_1980\",6378137,298.257222101]],         "
-                                 "PRIMEM[\"Greenwich\",0],        "
-                                 " UNIT[\"Degree\",0.017453292519943295]],     "
-                                 "PROJECTION[\"Albers_Conic_Equal_Area\"],     "
-                                 "PARAMETER[\"False_Easting\",0],    "
-                                 " PARAMETER[\"False_Northing\",0],     "
-                                 "PARAMETER[\"longitude_of_center\",-96],     "
-                                 "PARAMETER[\"Standard_Parallel_1\",20],     "
-                                 "PARAMETER[\"Standard_Parallel_2\",60],     "
-                                 "PARAMETER[\"latitude_of_center\",40],     "
-                                 "UNIT[\"Meter\",1],     "
-                                 "AUTHORITY[\"EPSG\",\"102008\"]]")
-
-        trans = osr.CoordinateTransformation(srsin,srsout)
-
-        layer = output_usm.CreateLayer('poly', srsout, ogr.wkbPolygon)
-
-        cd = mesh.GetCellData()
-
-
-        for i in range(0,cd.GetNumberOfArrays()):
-            # print cd.GetArrayName(i)
-            layer.CreateField(ogr.FieldDefn(cd.GetArrayName(i), ogr.OFTReal))
-
-        #build the triangulation geometery
-        for i in range(0, mesh.GetNumberOfCells()):
-            v0 = mesh.GetCell(i).GetPointId(0)
-            v1 = mesh.GetCell(i).GetPointId(1)
-            v2 = mesh.GetCell(i).GetPointId(2)
-
-            ring = ogr.Geometry(ogr.wkbLinearRing)
-            if is_geographic:
-                scale = 100000.  #undo the scaling that CHM does for the paraview output
-                ring.AddPoint( mesh.GetPoint(v0)[0] / scale, mesh.GetPoint(v0)[1] / scale)
-                ring.AddPoint (mesh.GetPoint(v1)[0]/ scale, mesh.GetPoint(v1)[1]/ scale )
-                ring.AddPoint( mesh.GetPoint(v2)[0]/ scale, mesh.GetPoint(v2)[1]/ scale )
-                ring.AddPoint( mesh.GetPoint(v0)[0]/ scale, mesh.GetPoint(v0)[1]/ scale ) # add again to complete the ring.
-
-                ring.Transform(trans) #only transform if needed
-
-            else:
-                ring.AddPoint( mesh.GetPoint(v0)[0], mesh.GetPoint(v0)[1] )
-                ring.AddPoint (mesh.GetPoint(v1)[0], mesh.GetPoint(v1)[1] )
-                ring.AddPoint( mesh.GetPoint(v2)[0], mesh.GetPoint(v2)[1] )
-                ring.AddPoint( mesh.GetPoint(v0)[0], mesh.GetPoint(v0)[1] ) # add again to complete the ring.
-
-            tpoly = ogr.Geometry(ogr.wkbPolygon)
-            tpoly.AddGeometry(ring)
-
-            feature = ogr.Feature( layer.GetLayerDefn() )
-            feature.SetGeometry(tpoly)
-
-            if variables is None:
-                for j in range(0, cd.GetNumberOfArrays()):
-                    name = cd.GetArrayName(j)
-                    variables.append(name)
-
-            for v in variables:
-                data = cd.GetArray(v).GetTuple(i)
-                feature.SetField(str(v), float(data[0]))
-
-            if parameters is not None:
-                for p in parameters:
-                    data = cd.GetArray(p).GetTuple(i)
-                    feature.SetField(str(p), float(data[0]))
-
-            layer.CreateFeature(feature)
-
-        x_min, x_max, y_min, y_max = layer.GetExtent()
-
-        NoData_value = -9999
-        x_res = int((x_max - x_min) / pixel_size)
-        y_res = int((y_max - y_min) / pixel_size)
-
-        for var in variables:
-            target_ds = gdal.GetDriverByName('GTiff').Create(output_path+'/'+vtu_file[:-4] + '_'+ var.replace(" ","_")+'.tif', x_res, y_res, 1, gdal.GDT_Float32)
-            target_ds.SetGeoTransform((x_min, pixel_size, 0, y_max, 0, -pixel_size))
-            # target_ds.SetProjection(srs)
-            band = target_ds.GetRasterBand(1)
-
-            band.SetNoDataValue(NoData_value)
-
-            # Rasterize
-            gdal.RasterizeLayer(target_ds, [1], layer, burn_values=[0],options=['ALL_TOUCHED=TRUE',"ATTRIBUTE="+var])
-            target_ds.SetProjection(srsout.ExportToWkt())
-            target_ds = None
-
-            # Optional clip file
-            if(constrain_flag):
-                subprocess.check_call(['gdalwarp -overwrite -s_srs \"%s\" -t_srs \"%s\" -te %f %f %f %f -r \"%s\" -tr %f %f \"%s\" \"%s\"' %
-                                       (srsout.ExportToProj4(),srsout.ExportToProj4(),o_xmin, o_ymin, o_xmax, o_ymax, var_resample_method[var], pixel_width, pixel_height, path[:-4]+'_'+var+'.tif',path[:-4]+'_'+var+'_clipped.tif')], shell=True)
-
-            if parameters is not None:
-                for p in parameters:
-                    target_ds = gdal.GetDriverByName('GTiff').Create(output_path+'/'+vtu_file[:-4] + '_'+ p.replace(" ","_") + '.tif', x_res, y_res, 1, gdal.GDT_Float32)
-                    target_ds.SetGeoTransform((x_min, pixel_size, 0, y_max, 0, -pixel_size))
-                    target_ds.SetProjection(srsout.ExportToWkt())
-                    band = target_ds.GetRasterBand(1)
-                    band.SetNoDataValue(NoData_value)
-
-                    # Rasterize
-                    gdal.RasterizeLayer(target_ds, [1], layer, burn_values=[0],options=['ALL_TOUCHED=TRUE', "ATTRIBUTE=" + p])
-                    target_ds = None
-
-                    # Optional clip file
-                    if(constrain_flag):
-                        subprocess.check_call([
-                          'gdalwarp -overwrite -s_srs \"%s\" -t_srs \"%s\" -te %f %f %f %f -r \"%s\" -tr %f %f \"%s\" \"%s\"' %
-                          (srsout.ExportToProj4(), srsout.ExportToProj4(), o_xmin, o_ymin, o_xmax,
-                           o_ymax, param_resample_method[p], pixel_width, pixel_height,
-                           output_path + '/' + vtu_file[:-4] + '_' + p.replace(" ","_") + '.tif',
-                           output_path + '/' + vtu_file[:-4] + '_' + p.replace(" ","_") + '_clipped.tif')],
-                          shell=True)
-
-        # we don't need to dump parameters for each timestep as they are currently assumed invariant with time.
-        parameters = None
-
-        #no parameters and no variables, just exit at this point
-        if not variables and parameters is None:
-            break
-
-        iter += 1
+	# add to master command file
+        f2.write('python '+para_ex+' '+new_config+'\n')
+    
+    f2.close()
 
 if __name__ == "__main__":
 
