af26b67a8a01873508b629e36a37c6a3fba98341
Chris Marsh <chrismarsh.c2@gmail.com>
2017-04-11 11:47:45 -0600

    add utc correct support for input forcing. change iswr_from_obs to use the method in SUMMA, Nijssen 1999 for splitting direct and diffuse

--- src/modules/interp_met/iswr_from_obs.cpp
+++ src/modules/interp_met/iswr_from_obs.cpp
@@ -10,6 +10,7 @@ iswr_from_obs::iswr_from_obs(config_file cfg)
 
     provides("iswr_direct_no_slope");
     provides("iswr_diffuse_no_slope");
+    provides("iswr_observed");
 
     provides("atm_trans");
 }
@@ -43,47 +44,26 @@ void iswr_from_obs::run(mesh_elem &face)
 
 
     auto query = boost::make_tuple(face->get_x(), face->get_y(), face->get_z());
-    double iswr_measured =face->get_module_data<data>(ID)->interp(lowered_values, query);
-
-    double splitting_coef = 0.0;
-
-    double elevation = face->face_data("solar_el");
-    double iswr_modeled = 1375.0; //top of atmosphere
-    double elevation_threshold = 3.0;
-
-    if( elevation < elevation_threshold ) {
-        //when the Sun is low above the horizon, Mt is getting abnormaly too large pretending
-        // this is a clear sky day when almost all the radiation should be diffuse
-        // no matter how the sky is
-        splitting_coef = 1.0;
-    } else {
-        // clear sky index (ratio global measured to top of atmosphere radiation)
-        const double Mt = iswr_measured / iswr_modeled; // should be <=1.2, aka clearness index Kt
-        const double clear_sky = 0.147;
-
-        // diffuse fraction: hourly ratio of diffuse to global radiation incident on a horizontal surface
-        // splitting according to a combination of Reindl et al.(1990)'s models (Mt-model and Mt&Psolar->elev-model):
-        if( Mt >= 0.78 ) { // Mt in [0.78;1] -> clear day
-            splitting_coef = clear_sky;
-        } else {
-            if( Mt <= 0.3 ) { // Mt in [0;0.3] -> overcast
-                splitting_coef = 1.02 - 0.248*Mt;
-                if(splitting_coef>1.) splitting_coef=1.;
-            } else {           // Mt in ]0.3;0.78[ -> cloudy
-                splitting_coef = 1.4 - 1.749*Mt + 0.177*sin(elevation*mio::Cst::to_rad);
-                if(splitting_coef>0.97) splitting_coef = 0.97;
-                if(splitting_coef<clear_sky) splitting_coef = clear_sky;
-            }
-        }
-    }
+    double iswr_observed =face->get_module_data<data>(ID)->interp(lowered_values, query);
 
 
-    double obs_dir = iswr_measured * splitting_coef;
-    double obs_diff = iswr_measured * (1-splitting_coef);
+    // This is what is used in SUMMA
+    //! compute the fraction of direct radiation using the parameterization of Nijssen and Lettenmaier (1999)
+    double Frad_direct = 0.7000;
+    double directScale = 0.0900;
+    double cosZenith = cos( M_PI/2.0 -  (face->face_data("solar_el")*mio::Cst::to_rad)) ; //zenith is from 90 vert -> 0 horz
+    double scalarFractionDirect = 0;
 
-    face->set_face_data("iswr_direct_no_slope",obs_dir);
-    face->set_face_data("iswr_diffuse_no_slope",obs_diff);
+    if(cosZenith > 0. )
+        scalarFractionDirect = Frad_direct*cosZenith/(cosZenith + directScale);
 
-    face->set_face_data("atm_trans",iswr_measured/1375.);
+    double split_dir = iswr_observed * scalarFractionDirect;
+    double split_diff = iswr_observed - split_dir;
+
+    face->set_face_data("iswr_direct_no_slope",split_dir);
+    face->set_face_data("iswr_diffuse_no_slope",split_diff);
+    face->set_face_data("iswr_observed",iswr_observed);
+
+    face->set_face_data("atm_trans",split_dir/1375.);
 }
 
