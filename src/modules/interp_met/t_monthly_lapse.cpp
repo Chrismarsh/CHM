241bdf23dc6611fa00f68bd48d0c856c536a8aa9
Chris Marsh <chrismarsh.c2@gmail.com>
2017-04-28 16:11:05 -0600

    refactor tlapse_monthly to improve speed

--- src/modules/interp_met/t_monthly_lapse.cpp
+++ src/modules/interp_met/t_monthly_lapse.cpp
@@ -25,53 +25,25 @@ void t_monthly_lapse::init(mesh domain)
         auto d = face->make_module_data<data>(ID);
         d->interp.init(global_param->interp_algorithm,global_param->get_stations( face->get_x(), face->get_y()).size());
     }
+
+    MLR[0]=cfg.get("MLR_1",0.0049);
+    MLR[1]=cfg.get("MLR_2",0.0049);
+    MLR[2]=cfg.get("MLR_3",0.0060);
+    MLR[3]=cfg.get("MLR_4",0.0060);
+    MLR[4]=cfg.get("MLR_5",0.0060);
+    MLR[5]=cfg.get("MLR_6",0.0053);
+    MLR[6]=cfg.get("MLR_7",0.0053);
+    MLR[7]=cfg.get("MLR_8",0.0053);
+    MLR[8]=cfg.get("MLR_9",0.0046);
+    MLR[9]=cfg.get("MLR_10",0.0046);
+    MLR[10]=cfg.get("MLR_11",0.0049);
+    MLR[11]=cfg.get("MLR_12",0.0049);
+
 }
 void t_monthly_lapse::run(mesh_elem& face)
 {
 
-    double lapse_rate = -9999;
-
-    switch(global_param->month())
-    {
-        case 1:
-            lapse_rate=cfg.get("MLR_1",0.0049);
-            break;
-        case 2:
-            lapse_rate=cfg.get("MLR_2",0.0049);
-            break;
-        case 3:
-            lapse_rate=cfg.get("MLR_3",0.0060);
-            break;
-        case 4:
-            lapse_rate=cfg.get("MLR_4",0.0060);
-            break;
-        case 5:
-            lapse_rate=cfg.get("MLR_5",0.0060);
-            break;
-        case 6:
-            lapse_rate=cfg.get("MLR_6",0.0053);
-            break;
-        case 7:
-            lapse_rate=cfg.get("MLR_7",0.0053);
-            break;
-        case 8:
-            lapse_rate=cfg.get("MLR_8",0.0053);
-            break;
-        case 9:
-            lapse_rate=cfg.get("MLR_9",0.0046);
-            break;
-        case 10:
-            lapse_rate=cfg.get("MLR_10",0.0046);
-            break;
-        case 11:
-            lapse_rate=cfg.get("MLR_11",0.0049);
-            break;
-        case 12:
-            lapse_rate=cfg.get("MLR_12",0.0049);
-            break;
-
-    }
-
+    double lapse_rate = MLR[global_param->month()-1];
 
     //lower all the station values to sea level prior to the interpolation
     std::vector< boost::tuple<double, double, double> > lowered_values;
--- src/modules/interp_met/t_monthly_lapse.hpp
+++ src/modules/interp_met/t_monthly_lapse.hpp
@@ -43,6 +43,7 @@ public:
     {
         interpolation interp;
     };
+    double MLR[12];
 };
 
 /**
