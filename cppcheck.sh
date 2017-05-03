d74f0607fff426ea192bd9973d9c2edc37be703d
Chris marsh <chrismarsh.c2@gmail.com>
2017-05-03 14:41:31 -0600

    small improvement for cppcheck

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cppcheck --enable=all `pwd`/src/ `pwd`/mesher/ --xml 2>err.xml
+cppcheck --language=c++ --enable=all `pwd`/src/ `pwd`/mesher/ --xml 2>err.xml
 ./cppcheck-htmlreport.py --file=err.xml --report-dir=`pwd`/cppcheck
 cd cppcheck
 open index.html
