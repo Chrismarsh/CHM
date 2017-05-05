06389b90479c95fab45132888ccb3308843ddc6c
Chris marsh <chrismarsh.c2@gmail.com>
2017-05-05 17:11:14 -0600

    new spline interpolation unit tests

--- src/tests/test_interpolation.cpp
+++ src/tests/test_interpolation.cpp
@@ -42,6 +42,28 @@ TEST_F(InterpTest,spline)
 
     ASSERT_LT(result,1);
 
+
+}
+
+TEST_F(InterpTest,spline2)
+{
+    thin_plate_spline s;
+    std::vector<boost::tuple<double,double,double> > xy;
+
+    xy.push_back( boost::make_tuple(-1276639.4142831599,1408220.6433826166,22.241299818717572));
+    xy.push_back( boost::make_tuple(-1276628.96002623, 1408213.5776356135, 22.423794697169313));
+    xy.push_back( boost::make_tuple(-1276628.8896492834,1408225.6645281466,22.301020204404736));
+
+    auto query = boost::make_tuple(-1276633.6294519969,1408220.6575855566,2306.0533040364585);
+
+    double z0 =  s(xy,query);
+    std::cout << z0 << std::endl;
+
+    double result = s(xy,query);
+
+    ASSERT_DOUBLE_EQ(result,22.301293951848109);
+
+
 }
 
 TEST_F(InterpTest,spline_forcedsize)
@@ -100,5 +122,26 @@ TEST_F(InterpTest,interpolation_class_static_size)
     double result = fabs(s(xy,query) - 15.795);
 
     ASSERT_LT(result,1);
+
+}
+
+TEST_F(InterpTest,interpolation_class_static_size2)
+{
+    interpolation s(interp_alg::tpspline,3);
+    std::vector<boost::tuple<double,double,double> > xy;
+
+    xy.push_back( boost::make_tuple(-1276639.4142831599,1408220.6433826166,22.241299818717572));
+    xy.push_back( boost::make_tuple(-1276628.96002623, 1408213.5776356135, 22.423794697169313));
+    xy.push_back( boost::make_tuple(-1276628.8896492834,1408225.6645281466,22.301020204404736));
+
+    auto query = boost::make_tuple(-1276633.6294519969,1408220.6575855566,2306.0533040364585);
+
+    double z0 =  s(xy,query);
+    std::cout << z0 << std::endl;
+
+    double result = s(xy,query);
+
+    ASSERT_DOUBLE_EQ(result,22.301293951848109);
+
 
 }
