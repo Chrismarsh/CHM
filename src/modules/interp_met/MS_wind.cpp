71c461e67d757d8b15a39b2da396a6b318e4f1ab
Kevin Green <kevin.richard.green@gmail.com>
2019-04-03 13:13:57 -0600

    Finish update OMP exceptions in MS_wind

--- src/modules/interp_met/MS_wind.cpp
+++ src/modules/interp_met/MS_wind.cpp
@@ -52,23 +52,28 @@ MS_wind::MS_wind(config_file cfg)
 //Calculates the curvature required
 void MS_wind::init(mesh& domain)
 {
+  ompException oe;
     #pragma omp parallel for
     for (size_t i = 0; i < domain->size_faces(); i++)
     {
         auto face = domain->face(i);
-        auto d = face->make_module_data<data>(ID);
-        d->interp.init(global_param->interp_algorithm,global_param->get_stations( face->get_x(), face->get_y()).size());
-        d->interp_smoothing.init(interp_alg::tpspline,3,{ {"reuse_LU","true"}});
+	oe.Run([&]
+	       {
+		 auto d = face->make_module_data<data>(ID);
+		 d->interp.init(global_param->interp_algorithm,global_param->get_stations( face->get_x(), face->get_y()).size());
+		 d->interp_smoothing.init(interp_alg::tpspline,3,{ {"reuse_LU","true"}});
+	       });
     }
+    oe.Rethrow();
 }
 
 
 void MS_wind::run(mesh& domain)
 {
+    ompException oe;
 
     if(!use_ryan_dir)
     {
-        ompException oe;
         #pragma omp parallel for
         for (size_t i = 0; i < domain->size_faces(); i++)
         {
@@ -78,26 +83,26 @@ void MS_wind::run(mesh& domain)
 		     std::vector<boost::tuple<double, double, double> > u;
 		     std::vector<boost::tuple<double, double, double> > v;
 		     for (auto &s : global_param->get_stations(face->get_x(), face->get_y()))
-		       {
-			 if (is_nan(s->get("U_R")) || is_nan(s->get("vw_dir")))
-			   continue;
+		     {
+		       if (is_nan(s->get("U_R")) || is_nan(s->get("vw_dir")))
+			 continue;
 
-			 double theta = s->get("vw_dir") * M_PI / 180.;
+		       double theta = s->get("vw_dir") * M_PI / 180.;
 
-			 double W = s->get("U_R");
-			 W = std::max(W, 0.1);
+		       double W = s->get("U_R");
+		       W = std::max(W, 0.1);
 
-			 W = Atmosphere::log_scale_wind(W,
-							Atmosphere::Z_U_R,  // UR is at our reference height
-							speedup_height,  // MS assumes a 2m wind speed
-							0); // no canopy, no snow, but uses a snow roughness
+		       W = Atmosphere::log_scale_wind(W,
+						      Atmosphere::Z_U_R,  // UR is at our reference height
+						      speedup_height,  // MS assumes a 2m wind speed
+						      0); // no canopy, no snow, but uses a snow roughness
 
-			 double zonal_u = -W * sin(theta);
-			 double zonal_v = -W * cos(theta);
+		       double zonal_u = -W * sin(theta);
+		       double zonal_v = -W * cos(theta);
 
-			 u.push_back(boost::make_tuple(s->x(), s->y(), zonal_u));
-			 v.push_back(boost::make_tuple(s->x(), s->y(), zonal_v));
-		       }
+		       u.push_back(boost::make_tuple(s->x(), s->y(), zonal_u));
+		       v.push_back(boost::make_tuple(s->x(), s->y(), zonal_v));
+		     }
 
 		     //http://mst.nerc.ac.uk/wind_vect_convs.html
 
@@ -169,32 +174,40 @@ void MS_wind::run(mesh& domain)
         #pragma omp parallel for
         for (size_t i = 0; i < domain->size_faces(); i++)
         {
-
             auto face = domain->face(i);
-            std::vector<boost::tuple<double, double, double> > u;
-            for (size_t j = 0; j < 3; j++)
-            {
-                auto neigh = face->neighbor(j);
-                if (neigh != nullptr && !neigh->_is_ghost)
-                    u.push_back(boost::make_tuple(neigh->get_x(), neigh->get_y(),(*neigh)["U_R"_s]));
-            }
+	    oe.Run([&]
+		   {
+		     std::vector<boost::tuple<double, double, double> > u;
+		     for (size_t j = 0; j < 3; j++)
+		     {
+		       auto neigh = face->neighbor(j);
+		       if (neigh != nullptr && !neigh->_is_ghost)
+			 u.push_back(boost::make_tuple(neigh->get_x(), neigh->get_y(),(*neigh)["U_R"_s]));
+		     }
 
-            double new_u = (*face)["U_R"_s];
+		     double new_u = (*face)["U_R"_s];
 
-            if(u.size()>0)
-            {
-                auto query = boost::make_tuple(face->get_x(), face->get_y(), face->get_z());
-                new_u = face->get_module_data<data>(ID)->interp_smoothing(u, query);
-            }
+		     if(u.size()>0)
+		     {
+		       auto query = boost::make_tuple(face->get_x(), face->get_y(), face->get_z());
+		       new_u = face->get_module_data<data>(ID)->interp_smoothing(u, query);
+		     }
 
-            face->get_module_data<data>(ID)->temp_u = new_u;
-        }
+		     face->get_module_data<data>(ID)->temp_u = new_u;
+		   });
+	}
+	oe.Rethrow();
+
         #pragma omp parallel for
         for (size_t i = 0; i < domain->size_faces(); i++)
         {
             auto face = domain->face(i);
-            (*face)["U_R"_s]= std::max(0.1, face->get_module_data<data>(ID)->temp_u);
+	    oe.Run([&]
+		   {
+		     (*face)["U_R"_s]= std::max(0.1, face->get_module_data<data>(ID)->temp_u);
+		   });
         }
+	oe.Rethrow();
     }else
     {
         // omega_s needs to be scaled on [-0.5,0.5]
@@ -204,156 +217,170 @@ void MS_wind::run(mesh& domain)
         for (size_t i = 0; i < domain->size_faces(); i++)
         {
             auto face = domain->face(i);
+	    oe.Run([&]
+		   {
+		     std::vector<boost::tuple<double, double, double> > u;
+		     std::vector<boost::tuple<double, double, double> > v;
+		     for (auto &s : global_param->get_stations(face->get_x(), face->get_y()))
+		     {
+		       if (is_nan(s->get("U_R")) || is_nan(s->get("vw_dir")))
+			 continue;
 
-            std::vector<boost::tuple<double, double, double> > u;
-            std::vector<boost::tuple<double, double, double> > v;
-            for (auto &s : global_param->get_stations(face->get_x(), face->get_y()))
-            {
-                if (is_nan(s->get("U_R")) || is_nan(s->get("vw_dir")))
-                    continue;
+		       double theta = s->get("vw_dir") * M_PI / 180.;
 
-                double theta = s->get("vw_dir") * M_PI / 180.;
+		       auto f = domain->face(s->closest_face());
+		       //figure out which lookup map we need
+		       int d = int(theta*180/M_PI/45.);
+		       if (d == 0) d = 8;
+		       double speedup = f->parameter("MS"+std::to_string(d));
 
-                auto f = domain->face(s->closest_face());
-                //figure out which lookup map we need
-                int d = int(theta*180/M_PI/45.);
-                if (d == 0) d = 8;
-                double speedup = f->parameter("MS"+std::to_string(d));
-
-                double W = s->get("U_R") / speedup;
-                W = std::max(W, 0.1);
-                W = Atmosphere::log_scale_wind(W,
-                                               Atmosphere::Z_U_R,  // UR is at our reference height
-                                               speedup_height,  // MS assumes a 2m wind speed
-                                               0); // no canopy, no snow, but uses a snow roughness
+		       double W = s->get("U_R") / speedup;
+		       W = std::max(W, 0.1);
+		       W = Atmosphere::log_scale_wind(W,
+						      Atmosphere::Z_U_R,  // UR is at our reference height
+						      speedup_height,  // MS assumes a 2m wind speed
+						      0); // no canopy, no snow, but uses a snow roughness
 
 
-                double zonal_u = -W * sin(theta);
-                double zonal_v = -W * cos(theta);
+		       double zonal_u = -W * sin(theta);
+		       double zonal_v = -W * cos(theta);
 
-                u.push_back(boost::make_tuple(s->x(), s->y(), zonal_u));
-                v.push_back(boost::make_tuple(s->x(), s->y(), zonal_v));
-            }
-    //http://mst.nerc.ac.uk/wind_vect_convs.html
+		       u.push_back(boost::make_tuple(s->x(), s->y(), zonal_u));
+		       v.push_back(boost::make_tuple(s->x(), s->y(), zonal_v));
+		     }
+		     //http://mst.nerc.ac.uk/wind_vect_convs.html
 
-            auto query = boost::make_tuple(face->get_x(), face->get_y(), face->get_z());
-            double zonal_u = face->get_module_data<data>(ID)->interp(u, query);
-            double zonal_v = face->get_module_data<data>(ID)->interp(v, query);
+		     auto query = boost::make_tuple(face->get_x(), face->get_y(), face->get_z());
+		     double zonal_u = face->get_module_data<data>(ID)->interp(u, query);
+		     double zonal_v = face->get_module_data<data>(ID)->interp(v, query);
 
-            double theta = 3.0 * M_PI * 0.5 - atan2(zonal_v, zonal_u);
+		     double theta = 3.0 * M_PI * 0.5 - atan2(zonal_v, zonal_u);
 
-            if (theta > 2.0 * M_PI)
-                theta = theta - 2.0 * M_PI;
+		     if (theta > 2.0 * M_PI)
+		       theta = theta - 2.0 * M_PI;
 
-            //eqn 15
-            double omega_s = face->slope() * cos(theta - face->aspect());
+		     //eqn 15
+		     double omega_s = face->slope() * cos(theta - face->aspect());
 
-            if (fabs(omega_s) > max_omega_s)
-                max_omega_s = fabs(omega_s);
+		     if (fabs(omega_s) > max_omega_s)
+		       max_omega_s = fabs(omega_s);
 
-            double W = sqrt(zonal_u * zonal_u + zonal_v * zonal_v);
+		     double W = sqrt(zonal_u * zonal_u + zonal_v * zonal_v);
 
-            face->get_module_data<data>(ID)->corrected_theta = theta;
-            face->get_module_data<data>(ID)->W = W;
+		     face->get_module_data<data>(ID)->corrected_theta = theta;
+		     face->get_module_data<data>(ID)->W = W;
+		   });
         }
+	oe.Rethrow();
+
         #pragma omp parallel for
         for (size_t i = 0; i < domain->size_faces(); i++)
         {
             auto face = domain->face(i);
+	    oe.Run([&]
+		   {
 
-            double theta= face->get_module_data<data>(ID)->corrected_theta;
-            double W= face->get_module_data<data>(ID)->W;
+		     double theta= face->get_module_data<data>(ID)->corrected_theta;
+		     double W= face->get_module_data<data>(ID)->W;
 
-            //what liston calls 'wind slope'
-            double omega_s = face->slope() * cos(theta - face->aspect());
+		     //what liston calls 'wind slope'
+		     double omega_s = face->slope() * cos(theta - face->aspect());
 
-            //scale between [-0.5,0.5]
-            omega_s = omega_s / (max_omega_s * 2.0);
+		     //scale between [-0.5,0.5]
+		     omega_s = omega_s / (max_omega_s * 2.0);
 
-            //scale wind here via MS
+		     //scale wind here via MS
 
-            double aspect = face->aspect();
-            double dirdiff = 0;
+		     double aspect = face->aspect();
+		     double dirdiff = 0;
 
-            double d2r = M_PI/180.0;
-
-
-            //Ryan terain divergence
-            // follow LIston's micromet implimentation to handle 0-360
-            if (aspect > 270.0*d2r &&  theta < 90.0*d2r)
-                dirdiff = aspect - theta - 360.0*d2r;
-            else if (aspect < 90.0*d2r && theta > 270.0*d2r)
-                dirdiff = aspect - theta + 360.0*d2r;
-            else
-                dirdiff = aspect - theta;
-
-            if (std::abs(dirdiff) < 90.0*d2r)
-            {
-                theta = theta - 0.5 * std::min(omega_s, 45.0*d2r) * sin((2.0 * dirdiff));
-                if (theta > 2.0*M_PI)
-                    theta = theta - 2*M_PI;
-                else if (theta < 0.0)
-                    theta = theta + 2.0*M_PI;
-
-            }
-
-            //figure out which lookup map we need
-            int d = int(theta*180.0/M_PI/45.);
-            if (d == 0) d = 8;
-
-            double speedup = face->parameter("MS"+std::to_string(d));
-            W = W*speedup;
-
-            W = std::max(W,0.1);
-            W = std::min(W,30.0);
-
-            W = Atmosphere::log_scale_wind(W,
-                                           speedup_height,  // MS assumes a 2m wind speed
-                                           Atmosphere::Z_U_R,  // UR is at our reference height
-                                           0); // no canopy, no snow, but uses a snow roughness
+		     double d2r = M_PI/180.0;
 
 
-            (*face)["U_R"_s]= W;
-            (*face)["vw_dir"_s]= theta * 180.0 / M_PI;
+		     //Ryan terain divergence
+		     // follow LIston's micromet implimentation to handle 0-360
+		     if (aspect > 270.0*d2r &&  theta < 90.0*d2r)
+		       dirdiff = aspect - theta - 360.0*d2r;
+		     else if (aspect < 90.0*d2r && theta > 270.0*d2r)
+		       dirdiff = aspect - theta + 360.0*d2r;
+		     else
+		       dirdiff = aspect - theta;
+
+		     if (std::abs(dirdiff) < 90.0*d2r)
+		     {
+		       theta = theta - 0.5 * std::min(omega_s, 45.0*d2r) * sin((2.0 * dirdiff));
+		       if (theta > 2.0*M_PI)
+			 theta = theta - 2*M_PI;
+		       else if (theta < 0.0)
+			 theta = theta + 2.0*M_PI;
+
+		     }
+
+		     //figure out which lookup map we need
+		     int d = int(theta*180.0/M_PI/45.);
+		     if (d == 0) d = 8;
+
+		     double speedup = face->parameter("MS"+std::to_string(d));
+		     W = W*speedup;
+
+		     W = std::max(W,0.1);
+		     W = std::min(W,30.0);
+
+		     W = Atmosphere::log_scale_wind(W,
+						    speedup_height,  // MS assumes a 2m wind speed
+						    Atmosphere::Z_U_R,  // UR is at our reference height
+						    0); // no canopy, no snow, but uses a snow roughness
 
 
-            Vector_2 v = math::gis::bearing_to_cartesian(theta* 180.0 / M_PI);
-            Vector_3 v3(-v.x(),-v.y(), 0); //negate as direction it's blowing instead of where it is from!!
+		     (*face)["U_R"_s]= W;
+		     (*face)["vw_dir"_s]= theta * 180.0 / M_PI;
 
-            face->set_face_vector("wind_direction",v3);
 
+		     Vector_2 v = math::gis::bearing_to_cartesian(theta* 180.0 / M_PI);
+		     Vector_3 v3(-v.x(),-v.y(), 0); //negate as direction it's blowing instead of where it is from!!
+
+		     face->set_face_vector("wind_direction",v3);
+		   });
         }
-
+	oe.Rethrow();
 
     #pragma omp parallel for
         for (size_t i = 0; i < domain->size_faces(); i++)
         {
 
             auto face = domain->face(i);
-            std::vector<boost::tuple<double, double, double> > u;
-            for (size_t j = 0; j < 3; j++)
-            {
-                auto neigh = face->neighbor(j);
-                if (neigh != nullptr && !neigh->_is_ghost)
-                    u.push_back(boost::make_tuple(neigh->get_x(), neigh->get_y(),(*neigh)["U_R"_s]));
-            }
+	    oe.Run([&]
+		   {
+		     std::vector<boost::tuple<double, double, double> > u;
+		     for (size_t j = 0; j < 3; j++)
+		     {
+		       auto neigh = face->neighbor(j);
+		       if (neigh != nullptr && !neigh->_is_ghost)
+			 u.push_back(boost::make_tuple(neigh->get_x(), neigh->get_y(),(*neigh)["U_R"_s]));
+		     }
 
 
-            double new_u = (*face)["U_R"_s];
-            if (u.size() > 0)
-            {
-                auto query = boost::make_tuple(face->get_x(), face->get_y(), face->get_z());
-                new_u = face->get_module_data<data>(ID)->interp_smoothing(u, query);
-            }
+		     double new_u = (*face)["U_R"_s];
+		     if (u.size() > 0)
+		     {
+		       auto query = boost::make_tuple(face->get_x(), face->get_y(), face->get_z());
+		       new_u = face->get_module_data<data>(ID)->interp_smoothing(u, query);
+		     }
 
-            face->get_module_data<data>(ID)->temp_u = new_u;
+		     face->get_module_data<data>(ID)->temp_u = new_u;
+		   });
         }
+	oe.Rethrow();
     #pragma omp parallel for
         for (size_t i = 0; i < domain->size_faces(); i++)
         {
             auto face = domain->face(i);
-            (*face)["U_R"_s]= std::max(0.1,face->get_module_data<data>(ID)->temp_u) ;
+	    oe.Run([&]
+		   {
+		     (*face)["U_R"_s]= std::max(0.1,face->get_module_data<data>(ID)->temp_u) ;
+		   });
         }
+	oe.Rethrow();
     }
 }
 
