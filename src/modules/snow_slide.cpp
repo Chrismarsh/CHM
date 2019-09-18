22244e44b8a2aaf7154ecaab7d54cd3d351ed451
Kevin Green <kevin.richard.green@gmail.com>
2019-09-18 17:19:08 -0600

    Add neighbour dependency to snow_slide

--- src/modules/snow_slide.cpp
+++ src/modules/snow_slide.cpp
@@ -27,7 +27,7 @@ REGISTER_MODULE_CPP(snow_slide);
 snow_slide::snow_slide(config_file cfg)
         : module_base("snow_slide", parallel::domain, cfg)
 {
-    depends("snowdepthavg");
+    depends("snowdepthavg",SpatialType::neighbour);
     depends("swe");
 
     use_vertical_snow = cfg.get("use_vertical_snow",true);
