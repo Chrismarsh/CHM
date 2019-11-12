6a6a50b26b162255f52dcf33b76a0482fd98c85e
Chris Marsh <chrismarsh.c2@gmail.com>
2019-11-12 14:52:35 -0600

    Initial metdata structure commit

---
+++ src/metdata.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Chris Marsh on 2019-11-12.
+//
+
+#include "metdata.hpp"
---
+++ src/metdata.hpp
@@ -0,0 +1,12 @@
+//
+// Created by Chris Marsh on 2019-11-12.
+//
+
+#ifndef CHMPRJ_METDATA_HPP
+#define CHMPRJ_METDATA_HPP
+
+class metdata
+{
+};
+
+#endif // CHMPRJ_METDATA_HPP
