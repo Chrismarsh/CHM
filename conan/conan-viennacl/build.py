4cc2d3afe09425706fab96ff3a6349241f6c72e3
Chris Marsh <chrismarsh.c2@gmail.com>
2019-11-22 16:02:58 -0600

    Switch to conan-viennacl submodule

--- conan/conan-viennacl/build.py
+++
@@ -1,6 +0,0 @@
-from conan.packager import ConanMultiPackager
-
-if __name__ == "__main__":
-    builder = ConanMultiPackager(username="bilke")
-    builder.add_common_builds()
-    builder.run()
--- conan/conan-viennacl/test/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <viennacl/scalar.hpp>
-
-int main(int argc, char* argv[])
-{
-	return 0;
-}
