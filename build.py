cf491f3745d98b10cf0c9fb544619f62519d613a
Chris Marsh <chrismarsh.c2@gmail.com>
2021-03-30 15:03:01 -0600

    update to per-OS CI

--- build.py
+++ build.py
@@ -2,26 +2,16 @@ from cpt.packager import ConanMultiPackager
 from collections import defaultdict
 import os
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     builder = ConanMultiPackager(cppstds=[14],
                                 archs=["x86_64"],
                                 build_types=["Release"])
+                        
+    builder.add_common_builds(pure_c=False, shared_option_name=False)
+    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") # old ABI
 
-                              
-    builder.add_common_builds(pure_c=False)
-
-    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
-
-    named_builds = defaultdict(list)
-    for settings, options, env_vars, build_requires, reference in builder.items:
-
-        if os.environ['USE_MPI'] == 'with-mpi':
-            options['CHM:with_mpi'] = True
-
-        named_builds[settings['compiler']].append([settings, options, env_vars, build_requires, reference])
-
-    builder.named_builds = named_builds
+    builder.update_build_if(lambda build: os.environ['USE_MPI'] == 'with-mpi',
+                        new_options={'CHM:with_mpi': False})
 
     builder.run()
-
