fe9e1d9c4e8b45966e40852685cc083711606aa1
Chris Marsh <chrismarsh.c2@gmail.com>
2021-03-30 15:35:13 -0600

    build policyh cap

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(cppstds=[14],
                                 archs=["x86_64"],
                                 build_types=["Release"],
-                                build_policy='Missing')
+                                build_policy='missing')
                         
     builder.add_common_builds(pure_c=False, shared_option_name=False)
     builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") # old ABI
