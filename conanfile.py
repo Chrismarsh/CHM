b28841c377ce4129ec1bce4e7114afb6c7577cef
Chris Marsh <chrismarsh.c2@gmail.com>
2021-09-04 22:20:17 -0400

    bump trilinos version to a custom version that incl required 13.x PRs

--- conanfile.py
+++ conanfile.py
@@ -80,7 +80,7 @@ class CHMConan(ConanFile):
         self.requires( "eigen3/[>=3.3.9]@CHM/stable" )
         self.requires( "meteoio/2.8.0@CHM/stable")
         self.requires( "func/0.1@CHM/stable")
-        self.requires( "trilinos/master@CHM/stable")
+        self.requires( "trilinos/chm@CHM/stable")
 
     def _configure_cmake(self):
         cmake = CMake(self)
