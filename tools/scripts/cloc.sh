95be7a3f8d612f70a34a91b7077b518638f34304
Chris Marsh <chrismarsh.c2@gmail.com>
2021-11-19 23:18:11 -0600

    fix cloc script

--- tools/scripts/cloc.sh
+++ tools/scripts/cloc.sh
@@ -1,2 +1,2 @@
 
-cloc --exclude-dir=CMakeFiles,matlab_support,meteoio,snowpack,CHM.wiki,cmake-build-debug,cmake-build-release,googletest,files  --exclude-lang=make,CMake,XML  src/ 
+cloc --exclude-dir=CMakeFiles,matlab_support,meteoio,snowpack,CHM.wiki,cmake-build-debug,cmake-build-release,googletest,files  --exclude-lang=make,CMake,XML  ../../src/ 
