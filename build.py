4bffc74fb7939584f6b581a22419ccb0102c3721
Chris Marsh <chrismarsh.c2@gmail.com>
2022-02-01 11:20:17 -0600

    update CI workflow

--- build.py
+++ build.py
@@ -6,8 +6,7 @@ import os
 if __name__ == "__main__":
     builder = ConanMultiPackager(cppstds=[14],
                                 archs=["x86_64"],
-                                build_types=["Release"],
-                                build_policy='missing')
+                                build_types=["Release"])
                         
     builder.add_common_builds(pure_c=False, shared_option_name=False)
     builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") # old ABI
