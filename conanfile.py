0369a5a913f5fa665411a03e01828d6addc2b8da
Chris Marsh <chrismarsh.c2@gmail.com>
2022-06-27 17:15:43 -0600

    update build reqs, use more from conan-io

--- conanfile.py
+++ conanfile.py
@@ -27,8 +27,26 @@ class CHMConan(ConanFile):
         "with_mpi": False,
 
         #dependency options
-        "gdal:libcurl": True,
-        "gdal:netcdf": True
+
+        "*:shared": True,
+
+        "gdal:shared": True,
+        "gdal:with_curl": True,
+        "gdal:with_netcdf": True,
+
+        "netcdf:shared": True,
+        "netcdf:dap": False,
+
+        "proj:with_curl": False,
+
+        "hdf5:shared": True,
+        "hdf5:enable_cxx": True,
+
+        "openblas:shared": True,
+        "openblas:build_lapack":True
+
+
+
         # "gperftools:heapprof":True
     }
 
@@ -65,13 +83,18 @@ class CHMConan(ConanFile):
 
 
     def requirements(self):
-
         self.requires( "cgal/[>=5.2]@CHM/stable" )
         self.requires( "boost/[>=1.75]@CHM/stable" )
         self.requires( "vtk/[>=9.0.1]@CHM/stable" )
-        self.requires( "netcdf-cxx/[>=4.3]@CHM/stable" )
-        self.requires( "proj/[>=7.2.1]@CHM/stable" )
-        self.requires( "gdal/[>=3.2.1]@CHM/stable" )
+
+        self.requires("netcdf-cxx/[>=4.3]@CHM/stable" )
+
+        self.requires( "proj/[>=7.2.1]" )
+        self.requires( "gdal/[>=3.2.1]" )
+
+        # Guide libtiff to make the right version selection
+        self.requires( "libdeflate/1.12",override=True)
+
         self.requires( "sparsehash/[>=2.0.3]@CHM/stable" )
         self.requires( "gperftools/[>=2.7]@CHM/stable" )
         self.requires( "gsl/[>=2.6]@CHM/stable" )
@@ -81,6 +104,7 @@ class CHMConan(ConanFile):
         self.requires( "meteoio/2.8.0@CHM/stable")
         self.requires( "func/0.1@CHM/stable")
         self.requires( "trilinos/chm@CHM/stable")
+
 
     def _configure_cmake(self):
         cmake = CMake(self)
