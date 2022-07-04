0b6d591b4a11fcdab509b46818e0fa2687efb5bc
Chris Marsh <chrismarsh.c2@gmail.com>
2022-07-04 15:15:54 -0600

    typo in eigen req

--- conanfile.py
+++ conanfile.py
@@ -90,7 +90,7 @@ class CHMConan(ConanFile):
         self.requires( "gsl/[>=2.6]@CHM/stable" )
         self.requires( "armadillo/[>=10.2.0]@CHM/stable" )
         self.requires( "onetbb/[>=2021.3.0]@CHM/stable" )
-        self.requires( "eigen3/[>=3.3.9]" )
+        self.requires( "eigen/[>=3.3.9]" )
         self.requires( "meteoio/2.8.0@CHM/stable")
         self.requires( "func/0.1@CHM/stable")
         self.requires( "trilinos/13.4.0@CHM/stable")
