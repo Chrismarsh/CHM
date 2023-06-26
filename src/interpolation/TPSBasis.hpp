df6bb3cf869b0a184376b4a2033e1629c958b073
Shawn McAdam <57196702+shawn-mcadam@users.noreply.github.com>
2023-06-26 15:49:45 -0600

    Using more digits of the Euler-Macheroni constant

--- src/interpolation/TPSBasis.hpp
+++ src/interpolation/TPSBasis.hpp
@@ -2,19 +2,19 @@
 #include <gsl/gsl_sf_expint.h>
 #include <cmath>
 
-// Needed if x>800 for old version. gsl_sf_expint_E1 overflows for x>800
+// the following two lines disables the overflow exception gsl_sf_expint_E1 throws for x>800
 //#include <gsl/gsl_errno.h>
 //static gsl_error_handler_t *old_error_handler=gsl_set_error_handler_off();
 
 template <typename T>
 T TPSBasis(T x){
-  T c = 0.577215;
+  T gamma = 0.57721566490153286060651209008240243104215933593992;
   // gsl_sf_expint_E1(x) < epsilon_double and ln(x) > 1 for x > 32
-  // so -(log(x) + c + gsl_sf_expint_E1(x)) == -(log(x) + c) is true for x > 32
-  return (x <= 32) ? -(log(x) + c + gsl_sf_expint_E1(x)) : -(log(x) + c);
+  // so -(log(x) + gamma + gsl_sf_expint_E1(x)) == -(log(x) + gamma) is true for x > 32
+  return (x <= 32) ? -(log(x) + gamma + gsl_sf_expint_E1(x)) : -(log(x) + gamma);
 }
 
-// TODO this function solely exists for timing purposes and should be removed
+// this function only exists for profiling
 template <typename T>
 T OLD_TPSBasis(T x){
   return -(log(x) + 0.577215 + gsl_sf_expint_E1(x));
