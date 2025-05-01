a0fb22744512150ccc581b17c5c1134340615ea7
Chris Marsh <chrismarsh.c2@gmail.com>
2025-05-01 14:28:52 -0600

    missing file

---
+++ src/math/ordinal_typedef.hpp
@@ -0,0 +1,35 @@
+//
+// Canadian Hydrological Model - The Canadian Hydrological Model (CHM) is a novel
+// modular unstructured mesh based approach for hydrological modelling
+// Copyright (C) 2018 Christopher Marsh
+//
+// This file is part of Canadian Hydrological Model.
+//
+// Canadian Hydrological Model is free software: you can redistribute it and/or
+// modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Canadian Hydrological Model is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Canadian Hydrological Model.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+
+#include <Tpetra_Core.hpp>
+
+
+
+
+typedef Tpetra::Details::DefaultTypes::global_ordinal_type global_ordinal_type;
+
+// essentially only ever used when a) the entire mesh is loaded or b) cell Id in MPI
+// but the mesh neded to exceed local_ordinal_type is too big for most machines
+typedef Tpetra::Details::DefaultTypes::local_ordinal_type local_ordinal_type;
