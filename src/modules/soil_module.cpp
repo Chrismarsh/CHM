7546282a10e1f268d8193b485f08632261f19d1a
Donovan Allum <donovan.allum@usask.ca>
2025-07-07 16:04:55 -0600

    Removed hard-coded porosity from soil_module
    In the CRHM output data from marmot creek, upper clearing, the soil
    module (SoilX) used a fixed value of porosity at 0.5.

--- src/modules/soil_module.cpp
+++ src/modules/soil_module.cpp
@@ -181,9 +181,8 @@ void soil_module::set_soil_params(mesh_elem& face, soil_module::data& d)
         
         d.pore_size_dist = face->soil_attribute<double>("PSD_K_estimator");
         d.pore_size_dist_organic = face->soil_attribute<double>("PSD_K_organic");
-        //const std::string soil_type = face->soil_attribute<std::string>("soil_type"_s,"soils");
-        d.porosity = 0.5; //SoilDataObj->porosity(soil_type);
-                          //TODO CRHM uses a fixed value for porosity, rather than searching in the SoilDataObj 
+        const std::string soil_type = face->soil_attribute<std::string>("soil_type"_s,"soils");
+        d.porosity = SoilDataObj->porosity(soil_type);
         d.soil_index = face->soil_attribute<double>("soil_index");
         d.snow_grain_diameter = face->soil_attribute<double>("snow_grain_diameter");
 
