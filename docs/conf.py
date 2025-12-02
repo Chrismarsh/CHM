ca7a103d262ef5df60a2bee1c44baed8a6fd43ae
Chris Marsh <chrismarsh.c2@gmail.com>
2025-12-02 06:04:47 +0000

    doc update theme

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/Chrismarsh/CHM",
+    "repository_branch": "develop",
+    "path_to_docs": "docs",
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "home_page_in_toc": False,
+    "logo": {
+        "image_light": "_static/images/CHM_logo.png",
+        "image_dark": "_static/images/CHM_logo.png",
+ 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
