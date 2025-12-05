7e48d224a97fc05a79e6e29da614070ff9a7a553
Allum <donovan.allum@usask.ca>
2025-12-05 10:13:50 -0700

    Correcting `base_step` constness, concepts, and constructors
    **Constness**
    
    `GuaranteeImplementExecute` now accepts `const T&` and `Data&` types.
    This ensures that (a) `T` has a function `void execute_impl` that
    takes a `Data&` and (b) `T` can be `const` and or a reference.
    
    **Concepts**
    
    The previous concept was circular: a derived class would need to be
    declared like:
    
    ```cpp
    template<typename Data>
    class Derived : public base_step<Derived,Data>
    ```
    
    But since `Derived` is not fully defined, it cannot satisfy the concept.
    Instead, this is replaced with a static assert that is only tripped if
    `execute` is called. It is still tripped at compile time and therefore
    has the same effect.
    
    **Constructors**
    
    The constructor is now explicitly declared and is protected.

--- src/modules/submodules/base_step.hpp
+++ src/modules/submodules/base_step.hpp
@@ -1,15 +1,19 @@
 #include <concepts>
 
 template<typename T, typename Data>
-concept GuaranteeImplementExecute = requires(T t, Data d) {
+concept GuaranteeImplementExecute = requires(const T& t, Data& d) {
     { t.execute_impl(d) } -> std::same_as<void>;
 };
 
 template<class Derived, class Data>
-    requires GuaranteeImplementExecute<Derived, Data>
 class base_step {
+protected:
+    base_step() {};
 public:
-    void execute(Data& d) {
-        static_cast<Derived*>(this)->execute_impl(d);
+    void execute(Data& d) const {
+        static_assert(GuaranteeImplementExecute<Derived,Data>,
+                "Derived class must implement: void execute_impl(Data&) const");
+        static_cast<const Derived*>(this)->execute_impl(d);
     }
+    ~base_step() {};
 };
