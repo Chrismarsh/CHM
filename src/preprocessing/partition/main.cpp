dde3cf06a4568cc16f9e93f446514bf55e634eb4
Allum <donovan.allum@usask.ca>
2025-12-10 16:46:35 -0700

    Removed variable that is set but unused

--- src/preprocessing/partition/main.cpp
+++ src/preprocessing/partition/main.cpp
@@ -141,11 +141,9 @@ class preprocessingTriangulation : public triangulation
         }
         // each processor only knows its own start and end indices
         size_t face_start_idx = 0;
-        size_t face_end_idx = _num_faces_in_partition.at(0) - 1;
         for (int i = 1; i <= _comm_world.rank(); ++i)
         {
             face_start_idx += _num_faces_in_partition.at(i - 1);
-            face_end_idx += _num_faces_in_partition.at(i);
         }
 
 #pragma omp parallel for
