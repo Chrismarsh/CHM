23550e510810b873a1705958c3d3dce63e54a8e6
Chris Marsh <chrismarsh.c2@gmail.com>
2026-01-09 11:17:01 -0600

    Remove redundant initialization of _mesh

--- src/mesh/ugrid_writer.cpp
+++ src/mesh/ugrid_writer.cpp
@@ -17,7 +17,6 @@
 #include "ugrid_writer.hpp"
 
 ugrid_writer::ugrid_writer(mesh m, std::shared_ptr<global> g, bool write_parameters, std::string fname, bool use_zarr):
-    _mesh(m),
     _fname(""),
     _store_path(std::move(fname)),
     _use_zarr(use_zarr),
